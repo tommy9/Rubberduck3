8005e1113a4a3be076574af484ab7cd7eb6e984c
Tom Helm <tomhelm@gmail.com>
2023-04-29 16:27:37 +0100

    Remove titlebar, add design data and tidy up

--- Shared/Rubberduck.UI/Abstract/Editor/IDialogShellViewModel.cs
+++ Shared/Rubberduck.UI/Abstract/Editor/IDialogShellViewModel.cs
@@ -1,25 +1,24 @@
-﻿using System.Drawing;
+﻿using System.Windows.Controls;
+using System.Windows.Media.Imaging;
 
 namespace Rubberduck.UI.Abstract
 {
     public interface IDialogShellViewModel
     {
         string TitleText { get; set; }
-        Image IconSource { get; set; }
+        BitmapImage IconSource { get; set; }
         string TitleLabelText { get; set; }
         string InstructionsLabelText { get; set; }
         string ContentsText { get; set; }
+        UserControl MainContentControl { get; set; }
+        string MoreInformationText { get; set; }
         string OptionLabelText { get; set; }
         bool OptionIsChecked { get; set; }
         string CancelButtonText { get; set; }
         string DefaultButtonText { get; set; }
-        //Whether or not to display the OptionLabel and bind OptionLabelText
         bool HasOption { get; set; }
-        //Whether or not to display the CancelButton and bind CancelButtonText
         bool HasCancelButton { get; set; }
-        //Whether or not to include minimize/maximize buttons in the control box, and whether the user can drag the border of the control to resize it
         bool CanResize { get; set; }
-        //Ensures a minimum breathing space for the client area when[re]sizing.
         int ClientAreaMinHeight { get; set; }
     }
 }
